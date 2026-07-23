65b3d706774da617823a512aaabdae6705ca7f68
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-07-23 14:30:37 -0500

    Configure geometry Monte Carlo fitting

---
+++ src/+mechanics/+config/geometryMonteCarloFitConfig.m
@@ -0,0 +1,12 @@
+function config = geometryMonteCarloFitConfig()
+%GEOMETRYMONTECARLOFITCONFIG Configuration for geometry-aware fit uncertainty.
+config.enabled = false;
+config.sampleCount = 200;
+config.confidenceLevel = 0.95;
+config.randomSeed = 1;
+config.minimumSuccessfulFraction = 0.8;
+config.initialLengthStd = NaN;
+config.initialAreaStd = NaN;
+config.refitNumberOfStarts = 2;
+config.storeFits = false;
+end
