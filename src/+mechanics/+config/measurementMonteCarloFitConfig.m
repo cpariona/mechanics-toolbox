0f2d279b8f70a4d832ef8c17aedeeab47ac421f6
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-07-23 21:50:23 -0500

    Define measurement Monte Carlo configuration directly

--- src/+mechanics/+config/measurementMonteCarloFitConfig.m
+++ src/+mechanics/+config/measurementMonteCarloFitConfig.m
@@ -1,4 +1,14 @@
 function config = measurementMonteCarloFitConfig()
 %MEASUREMENTMONTECARLOFITCONFIG Configuration for measurement-aware fit uncertainty.
-config = mechanics.config.geometryMonteCarloFitConfig();
+config.enabled = false;
+config.sampleCount = 200;
+config.confidenceLevel = 0.95;
+config.randomSeed = 1;
+config.minimumSuccessfulFraction = 0.8;
+config.initialLengthStd = NaN;
+config.initialAreaStd = NaN;
+config.forceStd = NaN;
+config.displacementStd = NaN;
+config.refitNumberOfStarts = 2;
+config.storeFits = false;
 end
