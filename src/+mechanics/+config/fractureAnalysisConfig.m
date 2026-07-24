952ada0ad97cf5eaa102f8f662a005dc84ee25fd
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-07-23 22:13:12 -0500

    Remove superseded fracture analysis configuration

--- src/+mechanics/+config/fractureAnalysisConfig.m
+++
@@ -1,6 +0,0 @@
-function config = fractureAnalysisConfig()
-%FRACTUREANALYSISCONFIG Default configuration for tensile peak metrics.
-config.enabled = true;
-config.integrateAbsoluteDisplacement = false;
-config.minimumObservations = 3;
-end
