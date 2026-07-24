8b3ee989f3cf56381cb2645a3319f6684d54fb1e
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-07-24 13:59:31 -0500

    Rename batch fitting context fields

--- src/+mechanics/+config/batchProcessingConfig.m
+++ src/+mechanics/+config/batchProcessingConfig.m
@@ -7,8 +7,8 @@ config.exportResults = false;
 config.outputFolder = "results";
 config.fitting.enabled = false;
 config.fitting.modelNames = ["neo-hookean", "mooney-rivlin", "yeoh"];
-config.fitting.context.inputMeasure = "engineering-strain";
-config.fitting.context.outputStressMeasure = "nominal";
+config.fitting.context.deformationMeasure = "engineering-strain";
+config.fitting.context.stressMeasure = "nominal";
 config.fitting.fitConfig = mechanics.config.fittingConfig();
 config.fitting.selectionConfig = mechanics.config.modelSelectionConfig();
 end
