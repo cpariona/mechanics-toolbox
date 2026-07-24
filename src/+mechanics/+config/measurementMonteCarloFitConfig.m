21b717eff287878628f4a836aae8acc57a576a1e
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-07-23 20:21:43 -0500

    Add measurement Monte Carlo config alias

---
+++ src/+mechanics/+config/measurementMonteCarloFitConfig.m
@@ -0,0 +1,4 @@
+function config = measurementMonteCarloFitConfig()
+%MEASUREMENTMONTECARLOFITCONFIG Configuration for measurement-aware fit uncertainty.
+config = mechanics.config.geometryMonteCarloFitConfig();
+end
