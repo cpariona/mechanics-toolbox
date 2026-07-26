75485269e04cb8215971dc8807ddd90bfe91be25
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-07-26 06:52:10 -0500

    Remove compression preload direction

--- src/+mechanics/+config/compressionConfig.m
+++ src/+mechanics/+config/compressionConfig.m
@@ -5,6 +5,5 @@ config.testType = "compression";
 config.preprocessing.branchMode = "manual";
 config.preprocessing.startIndex = 1;
 config.preprocessing.endIndex = Inf;
-config.preprocessing.zeroReference.loadingDirection = "decreasing";
 config.analysis.summaryStrainRange = [-0.05, 0.00];
 end
