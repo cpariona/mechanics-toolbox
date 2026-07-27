c90f4534ea01ea62203b892ffe47720fc32c0201
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-07-26 22:38:49 -0500

    Add shared plotting unit label formatter

---
+++ src/+mechanics/+plotting/formatUnitLabel.m
@@ -0,0 +1,13 @@
+function label = formatUnitLabel(name, unit)
+%FORMATUNITLABEL Append a bracketed unit to a plotting label.
+arguments
+    name (1,1) string
+    unit (1,1) string
+end
+
+if strlength(unit) == 0
+    label = name;
+else
+    label = name + " [" + unit + "]";
+end
+end
