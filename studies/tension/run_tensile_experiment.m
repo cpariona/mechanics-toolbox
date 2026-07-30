e683c01601c88cb70f9148ccb18df22380ceba36
Carlos Pariona <c.pariona@pucp.edu.pe>
2026-07-29 21:52:31 -0500

    drivers maximumRelativeParameterCV elimination

--- studies/tension/run_tensile_experiment.m
+++ studies/tension/run_tensile_experiment.m
@@ -88,7 +88,6 @@ fitting.selectionConfig.windowFractions = [0.50, 0.75, 1.00];
 fitting.selectionConfig.minimumObservations = 20;
 fitting.selectionConfig.rankingMetric = "BIC";
 fitting.selectionConfig.requireConvergence = true;
-fitting.selectionConfig.maximumRelativeParameterCV = 0.50;
 
 mc = fitting.measurementMonteCarlo;
 mc.enabled = false;
