b6a08e34fd5c9265dcd41ee3eefb7aeae8e68079
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-08-05 01:07:54 -0500

    refactor: use shared display units in stress strain plots

--- src/+mechanics/+plotting/plotStressStrain.m
+++ src/+mechanics/+plotting/plotStressStrain.m
@@ -5,11 +5,17 @@ arguments
     options.Title string = "Stress-strain curve"
     options.DisplayName string = "Experimental"
 end
+strainUnit = mechanics.plotting.mechanicalDisplayUnit( ...
+    "deformation", string(curve.units.strain));
+stressUnit = mechanics.plotting.mechanicalDisplayUnit( ...
+    "stress", string(curve.units.stress));
+
 figureHandle = figure("Color", "w");
-plot(curve.strain, curve.stress, "LineWidth", 1.5, "DisplayName", options.DisplayName);
+plot(curve.strain, curve.stress, "LineWidth", 1.5, ...
+    "DisplayName", options.DisplayName);
 grid on
-xlabel(sprintf("Strain [%s]", curve.units.strain));
-ylabel(sprintf("Stress [%s]", curve.units.stress));
+xlabel(mechanics.plotting.formatUnitLabel("Strain", strainUnit));
+ylabel(mechanics.plotting.formatUnitLabel("Stress", stressUnit));
 title(options.Title);
 legend("Location", "best");
 end
