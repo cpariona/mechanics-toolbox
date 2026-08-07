73c15e3688db98c88234cff7195dc7f19d7cd381
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-08-06 22:41:26 -0500

    Make Yeoh registry identities order-explicit

--- src/+mechanics/+models/modelRegistry.m
+++ src/+mechanics/+models/modelRegistry.m
@@ -10,6 +10,8 @@ switch normalizedName
     case {"neo-hookean", "neohookean"}
         model.name = "neo-hookean";
         model.displayName = "Neo-Hookean";
+        model.familyName = "neo-hookean";
+        model.order = NaN;
         model.functionHandle = @mechanics.models.neoHookean;
         model.parameterNames = "mu";
         model.defaultInitialGuess = 1;
@@ -22,6 +24,8 @@ switch normalizedName
     case {"mooney-rivlin", "mooneyrivlin"}
         model.name = "mooney-rivlin";
         model.displayName = "Mooney-Rivlin";
+        model.familyName = "mooney-rivlin";
+        model.order = NaN;
         model.functionHandle = @mechanics.models.mooneyRivlin;
         model.parameterNames = ["C10", "C01"];
         model.defaultInitialGuess = [0.5, 0.5];
@@ -35,6 +39,8 @@ switch normalizedName
     case "yeoh-second-order"
         model.name = "yeoh-second-order";
         model.displayName = "Yeoh second order";
+        model.familyName = "yeoh";
+        model.order = 2;
         model.functionHandle = @mechanics.models.yeoh;
         model.parameterNames = ["C10", "C20"];
         model.defaultInitialGuess = [1, 0];
@@ -44,9 +50,11 @@ switch normalizedName
         model.derivedQuantityNames = "mu0";
         model.evaluateDerivedQuantities = @(parameters) 2 * parameters(1);
 
-    case "yeoh"
-        model.name = "yeoh";
+    case "yeoh-third-order"
+        model.name = "yeoh-third-order";
         model.displayName = "Yeoh third order";
+        model.familyName = "yeoh";
+        model.order = 3;
         model.functionHandle = @mechanics.models.yeoh;
         model.parameterNames = ["C10", "C20", "C30"];
         model.defaultInitialGuess = [1, 0, 0];
